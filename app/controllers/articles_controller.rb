dc15bc0badfe0759e3a34f8a24a4bbbabe03ef24
Michele Dorazio <michele.dorazio96@gmail.com>
2021-05-03 19:35:37 +0200

    CRUD actions ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,26 @@ class ArticlesController < ApplicationController
     else
       render 'new' 
     end  
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was succesfully updated.'
+      redirect_to @article
+    else
+      render 'edit'
+    end  
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end  
 end  
 
