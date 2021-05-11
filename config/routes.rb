455fb8869c4b48b47c98038b7627209a9ad75115
Michele Dorazio <michele.dorazio96@gmail.com>
2021-05-11 19:24:00 +0200

    add route controller view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :articles
   get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
